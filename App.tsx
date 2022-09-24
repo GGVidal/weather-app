c65fdf3b4e2f11fd71b170fe20779c1178e9d586
GGVidal <gabrielvidalgondim@gmail.com>
2022-09-24 13:29:54 -0300

    feat: adding geolocation and google maps

--- App.tsx
+++ App.tsx
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import MapView from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import { LocationObject } from "expo-location";
 
 export default function App() {
+  const [location, setLocation] = useState<LocationObject>();
+  const [errorMsg, setErrorMsg] = useState("");
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let locationCoord = await Location.getCurrentPositionAsync({});
+      setLocation(locationCoord);
+    })();
+  }, []);
+  console.log("GG", location);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {location && (
+        <MapView
+          initialRegion={{
+            latitude: location.coords?.latitude,
+            longitude: location.coords?.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          style={styles.map}
+        />
+      )}
     </View>
   );
 }
@@ -13,8 +41,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
